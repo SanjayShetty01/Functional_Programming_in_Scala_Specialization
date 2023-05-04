4a65aedb8f992bafc85eeb74f93fd16fa60fe3d4
Sanjaya J Shetty <61221236+SanjayShetty01@users.noreply.github.com>
2023-05-04 21:02:07 +0530

    recFun solution

--- Course_1_Functional Programming Principles in Scala/Week_1/recfun/src/main/scala/recfun/RecFun.scala
+++ Course_1_Functional Programming Principles in Scala/Week_1/recfun/src/main/scala/recfun/RecFun.scala
@@ -20,9 +20,25 @@ object RecFun extends RecFunInterface:
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balancedHelper(chars: List[Char], open: Int): Boolean = chars match {
+      case Nil => open == 0
+      case '(' :: tail => balancedHelper(tail, open + 1)
+      case ')' :: tail => open > 0 && balancedHelper(tail, open - 1)
+      case _ :: tail => balancedHelper(tail, open)
+  }
+
+  balancedHelper(chars, 0)
+
+}
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 1
+    else if (money < 0 || coins.isEmpty) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
+
+
